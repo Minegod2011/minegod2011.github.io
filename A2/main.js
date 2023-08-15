778df381daf1305be2011cb88a1c21243c64f827
Minegod2011 <141151008+Minegod2011@users.noreply.github.com>
2023-08-16 00:12:49 +0530

    Delete A2 directory

--- A2/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    var images = ['pic 1.png', 'pic 2.png', 'pic 3.png', 'pic 4.png', 'pic 5.png', 'pic 6.png', 'pic 7.png', 'pic 8.png', 'pic 9.png', 'pic 10.png',];
-    var image = images[Math.floor(Math.random() * images.length)];
-    document.getElementsByTagName('body')[0].style.backgroundImage = "url('" + image + "')";
-    document.getElementsByTagName('body')[0].style.backgroundSize = "cover";
-    document.getElementsByTagName('body')[0].style.backgroundRepeat = "repeat-y";
-    document.getElementsByTagName('body')[0].style.backgroundPosition = "center";
-    document.getElementsByTagName('body')[0].style.backgroundAttachment = "fixed";
-}
